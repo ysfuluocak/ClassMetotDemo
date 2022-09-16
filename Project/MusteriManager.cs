3de893ea3ad73ec8a8f1e25f0f1e222d364448c2
ULUOCAK <ULUOCAK@DESKTOP-SBVS6PU>
2022-09-16 04:33:44 +0300

    Add project files.

---
+++ Project/MusteriManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri>();
+        int index = 0;
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi");            
+            musteriler.Add(musteri);                     
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            foreach (var item in musteriler)
+            {
+                if (musteri.Id == item.Id && musteri.Name == item.Name)
+                {
+                    index = item.Id - 1; 
+                }
+            }
+            musteriler.RemoveAt(index);
+            Console.WriteLine("Musteri Silindi!");
+        }
+
+        public void MusteriGetir()
+        {          
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Musteri adı : " + musteri.Name);
+            }
+        }
+
+        
+
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Project;
+
+MusteriManager musterimanager = new MusteriManager();
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Name = "Yusuf";
+musteri1.Surname = "ULUOCAK";
+musteri1.Email = "yszf@g.com";
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Name = "Ziya";
+musteri2.Surname = "kam";
+musteri2.Email = "fsgf@g.com";
+Musteri musteri3 = new Musteri();
+musteri3.Id = 3;
+musteri3.Name = "fasıl";
+musteri3.Surname = "Ucak";
+musteri3.Email = "mj45@g.com";
+musterimanager.MusteriEkle(musteri1);
+musterimanager.MusteriEkle(musteri2);
+musterimanager.MusteriEkle(musteri3);
+musterimanager.MusteriGetir();
+musterimanager.MusteriSil(musteri3);
+musterimanager.MusteriGetir();
